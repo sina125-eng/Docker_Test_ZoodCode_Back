c615b09131f0f64ce4b1dda1138a443fccfd1058
Sina <ssirinaa125@gmail.com>
2026-01-06 10:59:49 +0330

    error main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+
+app.enableCors({
+  origin: '*', // برای تست
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
+
+await app.listen(3000, '0.0.0.0');
+
 }
 bootstrap();
